7a3c6bc1b98f627d56d48fb827000302b9deea3c
marwa <marwajomaa4@gmail.com>
2020-04-15 00:26:52 +0300

    storing user data in app state

--- react-firebase/client/src/App.js
+++ react-firebase/client/src/App.js
@@ -14,8 +14,22 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)   
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+       if(userAuth){
+         const userRef = await createUserProfileDocument(userAuth);
+
+         userRef.onSnapshot(snapShot => {
+           this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+           }, () => console.log(this.state))
+         });
+       }
+       else if (!userAuth) {
+         this.setState({currentUser: userAuth}, () => console.log(this.state))
+       }  
     })
   }
 
--- react-firebase/client/src/firebase/firebase.utils.js
+++ react-firebase/client/src/firebase/firebase.utils.js
@@ -43,7 +43,7 @@ const config = {
 
        }
      }
-     
+     console.log(userRef);
      return userRef;
    }
 
